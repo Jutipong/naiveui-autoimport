67f777ce74ade0cbec7f08fc16d2ef0071f5c8f7
T660046@MacboookPro <T660046@tcrbank.com>
2024-12-27 11:23:20 +0700

    refactor: simplify navigation layout and improve component imports

--- src/components/@layouts/components/navigation/index.vue
+++ src/components/@layouts/components/navigation/index.vue
@@ -1,63 +1,26 @@
 <script lang="ts" setup>
 import { useAppStore } from '@/store/app'
-import BackTop from './../common/back-top.vue'
-import Footer from './../footer/index.vue'
-import HeaderComponent from './../header/index.vue'
-import LogoComponent from './logo.vue'
-import MenuComponent from './menu.vue'
+
+import Logo from './logo.vue'
+import Menu from './menu.vue'
 
 const appStore = useAppStore()
 </script>
 
 <template>
-    <n-layout class="wh-full" embedded has-sider>
-        <n-layout-sider
-            v-if="!appStore.contentFullScreen"
-            bordered
-            :collapsed="appStore.collapsed"
-            collapse-mode="width"
-            :collapsed-width="64"
-            :width="240"
-            content-style="display: flex;flex-direction: column;min-height:100%;"
-        >
-            <LogoComponent v-if="appStore.showLogo" />
+    <n-layout-sider
+        v-if="!appStore.contentFullScreen"
+        bordered
+        :collapsed="appStore.collapsed"
+        collapse-mode="width"
+        :collapsed-width="64"
+        :width="240"
+        content-style="display: flex;flex-direction: column;min-height:100%;"
+    >
+        <Logo v-if="appStore.showLogo" />
 
-            <n-scrollbar class="flex-1">
-                <MenuComponent />
-            </n-scrollbar>
-        </n-layout-sider>
-
-        <n-layout
-            class="h-full flex flex-col"
-            content-style="display: flex;flex-direction: column;min-height:100%;"
-            embedded
-            :native-scrollbar="false"
-        >
-            <HeaderComponent />
-
-            <div
-                class="flex flex-1 flex-col p-16px"
-                :class="{
-                    'p-t-121px': appStore.showTabs,
-                    'p-b-56px': appStore.showFooter && !appStore.contentFullScreen,
-                    'p-t-76px': !appStore.showTabs,
-                    'p-t-61px': appStore.contentFullScreen,
-                }"
-            >
-                <router-view v-slot="{ Component, route }" class="flex-1">
-                    <transition
-                        :name="appStore.transitionAnimation"
-                        mode="out-in"
-                    >
-                        <!-- <keep-alive :include="routeStore.cacheRoutes"> -->
-                        <component :is="Component" v-if="appStore.loadFlag" :key="route.fullPath" />
-                        <!-- </keep-alive> -->
-                    </transition>
-                </router-view>
-            </div>
-
-            <Footer />
-            <BackTop />
-        </n-layout>
-    </n-layout>
+        <n-scrollbar class="flex-1">
+            <Menu />
+        </n-scrollbar>
+    </n-layout-sider>
 </template>
--- src/components/@layouts/default.vue
+++ src/components/@layouts/default.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
+import { useAppStore } from '@/store/app'
+// import LogoComponent from './logo.vue'
+// import MenuComponent from './menu.vue'
+import BackTop from './components/common/back-top.vue'
 import SettingDrawer from './components/common/setting-drawer.vue'
+import Footer from './components/footer/index.vue'
+
+import HeaderComponent from './components/header/index.vue'
 import Navigation from './components/navigation/index.vue'
+
+const appStore = useAppStore()
 </script>
 
 <template>
     <SettingDrawer />
-    <Navigation />
+    <n-layout class="wh-full" embedded has-sider>
+        <!-- Navigation -->
+        <Navigation />
+
+        <n-layout class="h-full flex flex-col" content-style="display: flex;flex-direction: column;min-height:100%;" embedded :native-scrollbar="false">
+            <!-- HeaderComponent -->
+            <HeaderComponent />
+
+            <div class="flex flex-1 flex-col p-16px p-t-76px" :class="{ 'p-b-56px': appStore.showFooter }">
+                <router-view v-slot="{ Component, route }" class="flex-1">
+                    <transition :name="appStore.transitionAnimation" mode="out-in">
+                        <component :is="Component" v-if="appStore.loadFlag" :key="route.fullPath" />
+                    </transition>
+                </router-view>
+            </div>
+
+            <!-- Footer -->
+            <Footer />
+
+            <!-- BackTop -->
+            <BackTop />
+        </n-layout>
+    </n-layout>
 </template>
