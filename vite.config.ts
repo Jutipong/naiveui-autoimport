213360e19c595b81d17485f5a18070fbbd4d2d05
Jutipong.Dev <techcom.ec20@gmail.com>
2024-12-30 21:52:30 +0700

    refactor: remove unused plugin and proxy configurations, update Vite config for improved structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,93 @@
-import { resolve } from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
+import UnoCSS from '@unocss/vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
+import IconsResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig, loadEnv } from 'vite'
-import { createVitePlugins } from './build/plugins'
-import { createViteProxy } from './build/proxy'
-import { serviceConfig } from './service.config'
+import Layouts from 'vite-plugin-vue-layouts'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    // 根据当前工作目录中的 `mode` 加载 .env 文件
     const env = loadEnv(mode, __dirname, '') as ImportMetaEnv
-    const envConfig = serviceConfig[mode as ServiceEnvType]
-
     return {
         base: env.VITE_BASE_URL,
-        plugins: createVitePlugins(env),
+        plugins: [
+            vue(),
+            vueJsx(),
+            UnoCSS(),
+            AutoImport({
+                imports: [
+                    'vue',
+                    'vue-router',
+                    'pinia',
+                    '@vueuse/core',
+                    'vue-i18n',
+                    {
+                        'naive-ui': [
+                            'useDialog',
+                            'useMessage',
+                            'useNotification',
+                            'useLoadingBar',
+                            'useModal',
+                        ],
+                    },
+                ],
+                include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+                dirs: ['./src/utils/**', './src/stores', '!./src/utils/config/**'],
+                dts: './src/auto-imports.d.ts',
+            }),
+            Components({
+                dirs: ['src/components', '!./src/components/@layouts/**'],
+                dts: 'src/components.d.ts',
+                resolvers: [
+                    IconsResolver({
+                        prefix: false,
+                        customCollections: [
+                            'svg-icons',
+                        ],
+                    }),
+                    NaiveUiResolver(),
+                ],
+            }),
+            Layouts({
+                layoutsDirs: 'src/components/@layouts',
+                defaultLayout: 'default',
+            }),
+            VueRouter({
+                routesFolder: 'src/pages',
+                dts: 'src/typings/typed-router.d.ts',
+            }),
+            Icons({
+                defaultStyle: 'display:inline-block',
+                compiler: 'vue3',
+                customCollections: {
+                    'svg-icons': FileSystemIconLoader(
+                        'src/assets/svg-icons',
+                        svg => svg.replace(/^<svg /, '<svg fill="currentColor" width="1.2em" height="1.2em"'),
+                    ),
+                },
+            }),
+        ],
         resolve: {
             alias: {
-                '@': resolve(__dirname, 'src'),
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+                '@core': fileURLToPath(new URL('./src/@core', import.meta.url)),
+                '@assets': fileURLToPath(new URL('./assets', import.meta.url)),
+                '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+                '@images': fileURLToPath(new URL('./src/assets/images', import.meta.url)),
+                '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+                '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
             },
-        },
-        server: {
-            host: '0.0.0.0',
-            proxy:
-        env.VITE_HTTP_PROXY === 'Y' ? createViteProxy(envConfig) : undefined,
-        },
-        build: {
-            target: 'esnext',
-            reportCompressedSize: false, // 启用/禁用 gzip 压缩大小报告
+            extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
         },
         optimizeDeps: {
             include: ['echarts', 'md-editor-v3', 'quill'],
         },
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    api: 'modern',
-                },
-            },
-        },
+        css: { preprocessorOptions: { scss: { api: 'modern' } } },
     }
 })
